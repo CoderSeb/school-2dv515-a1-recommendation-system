0e83c1646f202710e8d40647b38ffa63d6b2a2f3
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2022-11-10 09:47:04 +0100

    Created frontend and backend boilerplate projects

---
+++ backend-assignment-1/src/main/java/lnu/sa224ny/backendassignment1/BackendAssignment1Application.java
@@ -0,0 +1,13 @@
+package lnu.sa224ny.backendassignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssignment1Application.class, args);
+	}
+
+}
---
+++ backend-assignment-1/src/test/java/lnu/sa224ny/backendassignment1/BackendAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lnu.sa224ny.backendassignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
